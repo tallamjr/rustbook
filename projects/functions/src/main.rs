e02b670eeac6397854ba914b50c2a09bf90f99a0
Tarek Allam <t.allam.jr@gmail.com>
2020-09-14 13:07:40 +0100

    Add new project 'branches'
    	new file:   branches/Cargo.toml
    	new file:   branches/src/main.rs
    	modified:   functions/src/main.rs

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
     another_function(5, 6);
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
 }
 
 fn another_function(x: i32, y: i32) {
