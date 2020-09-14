3a7c8ce7ba619a116b7eba5def27d556111dfc27
Tarek Allam <t.allam.jr@gmail.com>
2020-09-14 13:21:49 +0100

    Add new 'loops' projects
    	modified:   projects/branches/src/main.rs
    	new file:   projects/loops/Cargo.toml
    	new file:   projects/loops/src/main.rs

---
+++ projects/loops/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
