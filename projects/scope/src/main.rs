8e91c0ac0239d21d2cd14508f4d37fc55527bed9
Tarek Allam <t.allam.jr@gmail.com>
2020-09-15 09:47:06 +0100

    Add new project 'scope' for Chapter 4
    	new file:   scope/Cargo.toml
    	new file:   scope/src/main.rs

---
+++ projects/scope/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
+    // -----------------------------------------------------------------
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
+    // -----------------------------------------------------------------
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
+    // -----------------------------------------------------------------
+    let s = String::from("hello");  // s comes into scope
+
+    takes_ownership(s);             // s's value moves into the function...
+                                    // ... and so is no longer valid here
+
+    let x = 5;                      // x comes into scope
+
+    makes_copy(x);                  // x would move into the function,
+                                    // but i32 is Copy, so it’s okay to still
+                                    // use x afterward
+
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+}
