068e2b69ac2172c429c4969d0b45098f49e2147b
Tarek Allam <t.allam.jr@gmail.com>
2020-09-18 18:45:33 +0100

    MINOR
    	modified:   src/main.rs

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -9,12 +10,5 @@ fn main() {
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("rect1 is {:?}", rect1);
 }
