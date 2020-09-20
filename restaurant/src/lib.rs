e4ef38522bbc5273aa923beeb9b1edecb06d77be
Tarek Allam <t.allam.jr@gmail.com>
2020-09-20 16:15:04 +0100

    Chapter 7. Modules
    `cargo new --lib restaurant`
    
    	modified:   src/lib.rs

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -6,20 +6,34 @@ mod tests {
     }
 }
 
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+#![allow(unused)]
+fn main() {
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
     }
 }
 
-fn main() {}
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+}
+}
