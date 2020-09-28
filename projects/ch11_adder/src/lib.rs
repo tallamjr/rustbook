07f32f257a75e7b9fe507aa22210d17f8511eb9d
Tarek Allam <t.allam.jr@gmail.com>
2020-09-28 22:04:20 +0100

    Listing 11-8: Testing that a condition will cause a panic!
    	modified:   src/lib.rs

--- projects/ch11_adder/src/lib.rs
+++ projects/ch11_adder/src/lib.rs
@@ -1,12 +1,14 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+pub struct Guess {
+    value: i32,
 }
 
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
     }
 }
 
@@ -15,32 +17,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn larger_can_hold_smaller() {
-        // --snip--
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger));
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
 
