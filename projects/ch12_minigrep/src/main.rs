07773a5aad9ffa8c0299ade00081f4bed426d63e
Tarek Allam <t.allam.jr@gmail.com>
2020-09-30 22:38:27 +0100

    Listing 12-4: Reading the contents of the file specified by the second argument
    	new file:   poem.txt
    	modified:   src/main.rs

--- projects/ch12_minigrep/src/main.rs
+++ projects/ch12_minigrep/src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
+use std::fs;
 
 fn main() {
+    // --snip--
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
@@ -8,4 +10,9 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
