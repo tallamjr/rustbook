8d66a4a7a14588ab3930b362139ede113c69b73e
Tarek Allam <t.allam.jr@gmail.com>
2020-09-30 23:54:09 +0100

    Listing 12-7: Changing parse_config into Config::new

--- projects/ch12_minigrep/src/main.rs
+++ projects/ch12_minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -12,19 +12,23 @@ fn main() {
     let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
-    // --snip--
-
     println!("With text:\n{}", contents);
+
+    // --snip--
 }
+
+// --snip--
 
 struct Config {
     query: String,
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
