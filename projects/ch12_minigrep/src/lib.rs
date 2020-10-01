98c4c6d62b2b45d7e9d71d73d85d59085267bc7b
Tarek Allam <t.allam.jr@gmail.com>
2020-10-01 10:06:27 +0100

    Listing 12-15: Creating a failing test for the search function we wish we had

--- projects/ch12_minigrep/src/lib.rs
+++ projects/ch12_minigrep/src/lib.rs
@@ -8,7 +8,6 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        // --snip--
         if args.len() < 3 {
             return Err("not enough arguments");
         }
@@ -21,11 +20,25 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // --snip--
     let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
 
     Ok(())
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+fn main() {}
