4b46e1920a1368b53da77486705fea5f7adabba3
Tarek Allam <t.allam.jr@gmail.com>
2020-10-01 10:26:08 +0100

    Listing 12-24: Writing error messages to standard error instead of standard output using eprintln!

--- projects/ch12_minigrep/src/main.rs
+++ projects/ch12_minigrep/src/main.rs
@@ -1,23 +1,18 @@
 use std::env;
 use std::process;
 
-use ch12_minigrep::Config;
+use minigrep::Config;
 
 fn main() {
-    // --snip--
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    if let Err(e) = ch12_minigrep::run(config) {
-        // --snip--
-        println!("Application error: {}", e);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
